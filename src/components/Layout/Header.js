c34301def71592d96877f92dcf58e65547b6fa11
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2023-05-17 20:46:42 +0300

    modal conditional showing

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -5,12 +5,12 @@ import HeaderCartButton from "./HeaderCartButton";
 import classes from "./Header.module.css"; // gaining access with (classes.className) or (classes["className"])
 import background from "../../assets/meals.jpg";
 
-const Header = () => {
+const Header = props => {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>React Meals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes["main-image"]}>
                 <img src={background} alt="Background"/>
