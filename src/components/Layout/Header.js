05df37069bcc14fcdb8c7b33511dfcc7476d62ea
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2023-05-19 21:34:01 +0300

    remove/add item in cart & polishing

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,22 +1,21 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-import HeaderCartButton from "./HeaderCartButton";
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
-import classes from "./Header.module.css"; // gaining access with (classes.className) or (classes["className"])
-import background from "../../assets/meals.jpg";
-
-const Header = props => {
-    return (
-        <Fragment>
-            <header className={classes.header}>
-                <h1>React Meals</h1>
-                <HeaderCartButton onClick={props.onShowCart} />
-            </header>
-            <div className={classes["main-image"]}>
-                <img src={background} alt="Background"/>
-            </div>
-        </Fragment>
-    )   
-}
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
 
 export default Header;
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,28 +1,32 @@
-import { Fragment } from "react";
-import ReactDOM from "react-dom";
-import classes from "./Modal.module.css";
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 
-const Backdrop = props => {
-    return <div className={classes.backdrop} onClick={props.onClose}></div>
-}
+import classes from './Modal.module.css';
 
-const ModalOverlay = props => {
-   return ( 
-   <div className={classes.modal}>
-        <div className={classes.content}>{props.children}</div>
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
     </div>
-   )
-}
+  );
+};
 
-const Modal = props => {
-    const portalElement = document.getElementById("overlays");
+const portalElement = document.getElementById('overlays');
 
-    return (
-        <Fragment>
-            {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
-            {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
-        </Fragment>
-    )
-}
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
 
 export default Modal;
